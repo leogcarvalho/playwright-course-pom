d6d671cdf1848958a73c2ce1fef4d76c02bca3c6
Leonardo Carvalho <leo.leocarvalho@gmail.com>
2025-11-25 08:09:27 -0300

    Adicionando novos casos de teste

---
+++ pages/common_page.py
@@ -0,0 +1,13 @@
+from playwright.sync_api import Page, expect
+
+class CommonPage:
+    def __init__(self, page: Page):
+        self.page = page
+        self.voltar_para_home = page.get_by_role("button", name="Voltar para a Home")
+
+    def assert_text(self, text):
+        self.page.get_by_text(text).wait_for()
+        expect(self.page.get_by_text(text)).to_be_visible()
+
+    def voltar_home(self):
+        self.voltar_para_home.click()
